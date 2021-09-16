a959ae9f4bf6178878592589f1942ea9980f7e86
Buldin Alexandr <buldin.alieksandr@gmail.com>
2021-09-16 18:49:48 +0300

    add splash screen

--- app/src/main/java/com/example/rustcal/SplashActivity.java
+++ app/src/main/java/com/example/rustcal/SplashActivity.java
@@ -2,13 +2,32 @@ package com.example.rustcal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGHT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+      // костыльный LaunchScreen
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_DISPLAY_LENGHT);
+
+        // можно будет переделать на это
+        /*Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();*/
+
+
     }
 }
