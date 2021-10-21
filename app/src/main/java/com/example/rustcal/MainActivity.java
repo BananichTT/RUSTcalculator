05ae89a892de9d8d3b522465d27f466f65cefd8d
Buldin Alexandr <buldin.alieksandr@gmail.com>
2021-10-21 18:32:46 +0300

    db tests

--- app/src/main/java/com/example/rustcal/MainActivity.java
+++ app/src/main/java/com/example/rustcal/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.rustcal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button read = (Button)findViewById(R.id.button);
+        Button insert = (Button)findViewById(R.id.button2);
+
+        RustDB rustDB = new RustDB(this);
+
+        SQLiteDatabase database = rustDB.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contentValues.put(RustDB.KEY_NAME, "Rock");
+                contentValues.put(RustDB.KEY_DAMAGE, 10);
+                contentValues.put(RustDB.KEY_ITEM_STRENGTH, 500);
+
+                database.insert(RustDB.TABLE_CONTACTS, null, contentValues);
+            }
+        });
+
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor cursor = database.query(RustDB.TABLE_CONTACTS, null, null,null,null, null,null);
+
+                if(cursor.moveToFirst()){
+
+                    int nameIndex = cursor.getColumnIndex(RustDB.KEY_NAME);
+                    int damageIndex = cursor.getColumnIndex(RustDB.KEY_DAMAGE);
+                    int itemsIndex = cursor.getColumnIndex(RustDB.KEY_ITEM_STRENGTH);
+                    do {
+                        Log.d("mLog",
+                                ", name = " + cursor.getString(nameIndex) +
+                                ", damage = " + cursor.getString(damageIndex) +
+                                ", itemS = " + cursor.getInt(itemsIndex));
+                    }while (cursor.moveToNext());
+                }else{
+                    Log.d("mLog", "0 rows");
+                    cursor.close();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/rustcal/RustDB.java
+++ app/src/main/java/com/example/rustcal/RustDB.java
@@ -7,17 +7,25 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class RustDB extends SQLiteOpenHelper {
-    public RustDB(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "rustDb";
+    public static final String TABLE_CONTACTS = "items";
+
+    public static final String KEY_NAME = "name";
+    public static final String KEY_DAMAGE = "damage";
+    public static final String KEY_ITEM_STRENGTH = "items";
+
+    public RustDB(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_NAME + " text primary key," + KEY_DAMAGE + "text," + KEY_ITEM_STRENGTH + "text" + ")");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("drop table if exists " + TABLE_CONTACTS);
     }
 }
