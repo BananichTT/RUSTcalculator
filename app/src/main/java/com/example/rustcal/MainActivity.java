1a7a0bc1771b8326220bd8d8667670a69b97b948
Buldin Alexandr <buldin.alieksandr@gmail.com>
2021-10-22 20:44:27 +0300

    working SQLiteDataBase

--- app/src/main/java/com/example/rustcal/MainActivity.java
+++ app/src/main/java/com/example/rustcal/MainActivity.java
@@ -2,61 +2,58 @@ package com.example.rustcal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.rustcal.ui.DatabaseHelper;
+
+import java.io.IOException;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseHelper DBHelper;
+    private SQLiteDatabase Db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button read = (Button)findViewById(R.id.button);
-        Button insert = (Button)findViewById(R.id.button2);
+        DBHelper = new DatabaseHelper(this);
 
-        RustDB rustDB = new RustDB(this);
+        try {
+            DBHelper.updateDataBase();
+        } catch (IOException mIOException) {
+            throw new Error("UnableToUpdateDatabase");
+        }
 
-        SQLiteDatabase database = rustDB.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-
-
-
-        insert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                contentValues.put(RustDB.KEY_NAME, "Rock");
-                contentValues.put(RustDB.KEY_DAMAGE, 10);
-                contentValues.put(RustDB.KEY_ITEM_STRENGTH, 500);
-
-                database.insert(RustDB.TABLE_CONTACTS, null, contentValues);
-            }
-        });
+        try {
+            Db =DBHelper.getWritableDatabase();
+        } catch (SQLException mSQLException) {
+            throw mSQLException;
+        }
 
         read.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Cursor cursor = database.query(RustDB.TABLE_CONTACTS, null, null,null,null, null,null);
+                String items = "";
 
-                if(cursor.moveToFirst()){
+                Cursor cursor = Db.rawQuery("SELECT * FROM items", null);
+                cursor.moveToFirst();
 
-                    int nameIndex = cursor.getColumnIndex(RustDB.KEY_NAME);
-                    int damageIndex = cursor.getColumnIndex(RustDB.KEY_DAMAGE);
-                    int itemsIndex = cursor.getColumnIndex(RustDB.KEY_ITEM_STRENGTH);
-                    do {
-                        Log.d("mLog",
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", damage = " + cursor.getString(damageIndex) +
-                                ", itemS = " + cursor.getInt(itemsIndex));
-                    }while (cursor.moveToNext());
-                }else{
-                    Log.d("mLog", "0 rows");
-                    cursor.close();
+                while (!cursor.isAfterLast()){
+                    items += cursor.getString(0) + " | ";
+                    cursor.moveToNext();
                 }
+                cursor.close();
+
+                Log.d("mLog", items);
             }
         });
     }
